a3b974d8c99bf2b4cdceb4148ba91a884a5f2c55
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2013-12-17 22:13:13 -0500

    updated to use superagent

--- promises/es6-promises.js
+++ promises/es6-promises.js
@@ -1,20 +1,31 @@
-
 // var promise = require('es6-promise');
 var promise = require('es6-promise');
+var request = require('superagent');
+
 
 var p = new promise.Promise(function(resolve, reject) {
-  // do a thing, possibly async, then…
-  
-  if (true) {
-    resolve("Stuff worked!");
-  }
-  else {
-    reject(Error("It broke"));
-  }
+	// do a thing, possibly async, then…
+
+	request
+		.get('http://localhost:4730')
+		.end(function(res) {
+			if (res) {
+				resolve(res);
+			} else {
+				reject(Error("It broke"));
+			}
+		});
+
+	// if (true) {
+	//   resolve("Stuff worked!");
+	// }
+	// else {
+	//   reject(Error("It broke"));
+	// }
 });
 
 p.then(function(result) {
-  console.log(result); // "Stuff worked!"
+	console.log(result.text); // "Stuff worked!"
 }, function(err) {
-  console.log(err); // Error: "It broke"
+	console.log(err); // Error: "It broke"
 });
