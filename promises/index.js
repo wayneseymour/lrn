789af579f6426b4249f0a590d5760ed37e0815f5
W. K. Seymour III <wayneseymour@gmail.com>
2013-12-17 22:28:18 -0500

    init commit

---
+++ promises/index.js
@@ -0,0 +1,30 @@
+var promise = require('es6-promise');
+var request = require('superagent');
+
+
+var p = new promise.Promise(function(resolve, reject) {
+	// do a thing, possibly async, then…
+
+	request
+		.get('http://jsonip.com')
+		.end(function(res) {
+			if (res) {
+				resolve(res);
+			} else {
+				reject(Error("It broke"));
+			}
+		});
+
+	// if (true) {
+	//   resolve("Stuff worked!");
+	// }
+	// else {
+	//   reject(Error("It broke"));
+	// }
+});
+
+p.then(function(result) {
+	console.log(result.text); // "Stuff worked!"
+}, function(err) {
+	console.log(err); // Error: "It broke"
+});
