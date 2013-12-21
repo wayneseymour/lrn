6f103f1fca25925928b86dd45b7fc84cb369dc7c
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2013-12-21 14:15:08 -0500

    merged

---
+++ promises/index.js
@@ -0,0 +1,27 @@
+var promise = require('es6-promise');
+var request = require('superagent');
+
+
+var p = new promise.Promise(function(resolve, reject) {
+	// do a thing, possibly async, then…
+    var url = 'http://jsonip.com';
+    
+    console.log("\n### executing rest call to: \n\n\t'%s'", url);
+    
+	request
+		.get(url)
+		.end(function(res) {
+			if (res) {
+				resolve(res);
+			} else {
+				reject(Error("It broke"));
+			}
+		});
+
+});
+// eric
+p.then(function(result) {
+	console.log("\n### rest call result: \n\n\t%s\n", result.text); // "Stuff worked!"
+}, function(err) {
+	console.log(err); // Error: "It broke"
+});
