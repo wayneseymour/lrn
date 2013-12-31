1f551c4ebe4a3d8121708cea511c9338f8dae9aa
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2013-12-31 15:56:12 -0500

    Following along but noticed the last print didnt work unless I returned, contrary to the documentation @: https://code.google.com/p/selenium/wiki/WebDriverJs

--- webdriverjs/webdriver-js-spike/promises-style.js
+++ webdriverjs/webdriver-js-spike/promises-style.js
@@ -8,9 +8,10 @@ driver.get("http://www.google.com");
 driver.findElement(webdriver.By.name("q")).sendKeys("webdriver");
 driver.findElement(webdriver.By.name("btnG")).click();
 driver.getTitle().then(function(title) {
-	// assertEquals("webdriver - Google Search", title);
-
+	console.log("title is: " + title);
+	return title;
+}).then(function(title) {
+	console.log("title still is: " + title);
+}).then(function(title) {
 	driver.quit();
 });
-
-
