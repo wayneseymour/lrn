dc48e0824b34433079e7792dd8837952ce5131ba
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2014-01-29 06:46:49 -0500

    finally figured out how to make a new controller

---
+++ angular/ng-spike/app/scripts/controllers/text.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngSpikeApp')
+  .controller('TextCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Benny',
+      'Brian',
+      'Cesar'
+    ];
+  });
