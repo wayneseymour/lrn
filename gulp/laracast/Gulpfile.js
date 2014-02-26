9eef229239e779183fa75a825323d5f64e58e876
wayneseymour <--global>
2014-02-26 16:36:27 -0500

    researching gulp.js

---
+++ gulp/laracast/Gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function () {
+	return gulp.src('css/main.css')
+		.pipe(autoprefixer())
+		.pipe(minifycss())
+		.pipe(gulp.dest('css/min'));
+});
+
+gulp.task('default', function() {
+	gulp.run('css');
+
+
+	gulp.watch('css/*.css', function() {
+		gulp.run('css');
+	})
+})
