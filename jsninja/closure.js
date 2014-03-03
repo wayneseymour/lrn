e0ea5603c3700bf36801e160d405b1ed36cbfa6f
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2014-03-03 10:43:18 -0600

    works and looks cleaner with Array.forEach.  It just reads so much cleaner.

--- jsninja/closure.js
+++ jsninja/closure.js
@@ -17,13 +17,21 @@
 // }
 
 //with closure, due to iife
-for (var i = 0; i < 10; i++) {
-    setTimeout(
-        (function(localToThis) {
-            return function() {
-                console.log(localToThis);
-            };
-        })(i) // We define and call simultaneously.
-        , 100);
-}
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(
+//         (function(localToThis) {
+//             return function() {
+//                 console.log(localToThis);
+//             };
+//         })(i) // We define and call simultaneously.
+//         , 100);
+// }
 
+// with forEach (intrinsic closure)
+var zeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+zeroToNine.forEach(function(i) {
+    setTimeout(function() {
+        console.log(i);
+    }, 100);
+});
