7b61b03378a176ed3f8267e3b239deb3f1f9e639
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2014-03-03 10:40:51 -0600

    works with an iife and closure, but looks a bit messy.

--- jsninja/closure.js
+++ jsninja/closure.js
@@ -7,12 +7,23 @@
 // }
 
 // with closure
+// for (var i = 0; i < 10; i++) {
+//     var logIndexClosure = function(localToThis) {
+//         return function() {
+//             console.log(localToThis);
+//         };
+//     };
+//     setTimeout(logIndexClosure(i), 100); // Logs 0 to 9 real fast!
+// }
+
+//with closure, due to iife
 for (var i = 0; i < 10; i++) {
-    var logIndexClosure = function(localToThis) {
-        return function() {
-            console.log(localToThis);
-        };
-    };
-    setTimeout(logIndexClosure(i), 100); // Logs 0 to 9 real fast!
+    setTimeout(
+        (function(localToThis) {
+            return function() {
+                console.log(localToThis);
+            };
+        })(i) // We define and call simultaneously.
+        , 100);
 }
 
