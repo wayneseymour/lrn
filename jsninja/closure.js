952553217e4578fa7b706816568e87a50a3514ba
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2014-03-03 10:40:03 -0600

    works with a closure, but now I have a ref to a var I only use once.

--- jsninja/closure.js
+++ jsninja/closure.js
@@ -1,7 +1,18 @@
 // without closure
+// for (var i = 0; i < 10; i++) {
+//     logIndex = function() {
+//         console.log(i);
+//     };
+//     setTimeout(logIndex, 100); // Logs 0 to 9 real fast... right?
+// }
+
+// with closure
 for (var i = 0; i < 10; i++) {
-    logIndex = function() {
-        console.log(i);
+    var logIndexClosure = function(localToThis) {
+        return function() {
+            console.log(localToThis);
+        };
     };
-    setTimeout(logIndex, 100); // Logs 0 to 9 real fast... right?
+    setTimeout(logIndexClosure(i), 100); // Logs 0 to 9 real fast!
 }
+
