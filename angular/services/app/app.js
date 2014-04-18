777276197210d2106a0d8b9949b485c679e92254
--global <wayneseymour@gmail.com>
2014-04-18 09:14:35 -0400

    Got the first service wired up.

--- angular/services/app/app.js
+++ angular/services/app/app.js
@@ -1,5 +1,23 @@
-function FirstCtrl($scope) {
+'use strict';
+
+var todoApp = angular.module('todoApp', []);
+
+todoApp.controller('FirstController', function($scope) {
   $scope.data = {
     message: "Hello"
   };
-}
+});
+
+todoApp.controller('GoodbyeController',
+  function($scope, testService) {
+    $scope.fromService = testService.sayHello("Wayne");
+  });
+
+todoApp.service('testService', function() {
+  this.sayHello = function(text) {
+    return "Service says \"Hello " + text + "\"";
+  };
+  this.sayGoodbye = function(text) {
+    return "Service says \"Goodbye " + text + "\"";
+  };
+});
